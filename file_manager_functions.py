e265726cfa4044423680e8c561e0ee8f3f75f80b
anastasia <anastasiazorina2299@gmail.com>
2025-03-17 12:12:16 +0300

    Updating functions and CLI

--- file_manager_functions.py
+++ file_manager_functions.py
@@ -5,19 +5,15 @@ from datetime import datetime
 
 
 # Copy file
-def copy_file(filename, directory_name):
-    full_file_path = os.path.join(directory_name, filename)
-    destination_path = input("Where to copy the file to? ")
-
-    if os.path.isfile(full_file_path):
+def copy_file(full_path, destination_path):
+    if os.path.isfile(full_path):
         try:
-            shutil.copy2(full_file_path, destination_path)
-            print(f"The file {filename} was successfully copied! Check that out!")
+            shutil.copy2(full_path, destination_path)
+            print("The file was successfully copied! Check that out!")
         except:
-            print(f"Error copying the file {filename}")
-
+            print("Error copying the file")
     else:
-        print(f"The file {filename} could not be found!")
+        print(f"The file could not be found!")
 
 
 # Remove file or directory
